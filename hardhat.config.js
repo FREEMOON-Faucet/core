3133c4bbdb895aedd6c69f3556db29fec342707b
Paddy Curé <paddy.cure@gmail.com>
2021-07-29 12:13:16 +0100

    deploy tokens ready

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.5",
   networks: {
     fsnTestnet: {
-      url: "https://testway.freemoon.xyz/gate",
+      url: "https://testnet.anyswap.exchange",
       chainId: 46688,
       accounts: [ process.env.PRIVATE_KEY ]
     },
     fsnMainnet: {
-      url: "https://mainnet.anyswap.exchange",
+      url: "https://fsn.dev/api",
       chainId: 32659,
       accounts: [ process.env.ADMIN_PRIVATE ]
     }
---
+++ scripts/47_set_minters.js
@@ -0,0 +1,1 @@
+const FREE = artifacts.require("FREE")
