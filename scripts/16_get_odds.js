92589081f28cccedd4b4c7c1e068e37dfecdddf2
Paddy Curé <paddy.cure@gmail.com>
2022-01-21 17:47:10 +0000

    check odds

--- scripts/16_get_odds.js
+++ scripts/16_get_odds.js
@@ -12,15 +12,22 @@ const getOdds = async () => {
   let odds = []
 
   for(let i = 0; i < 8; i++) {
-    categories.push(web3.utils.fromWei(await faucet.categories(i)))
+    let category = web3.utils.fromWei(await faucet.categories(i))
+    let odd = (await faucet.odds(i)).toString()
+    categories.push(category.replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+    odds.push(odd.replace(/\B(?=(\d{3})+(?!\d))/g, ","))
   }
 
-  for(let i = 0; i < 8; i++) {
-    odds.push((await faucet.odds(i)).toString())
+  // Log the odds for category 0
+  console.log(`0: <${ categories[ 0 ] } FREE: ${ odds[ 0 ] }`)
+
+  // Log the odds for category 1-6
+  for(let i = 1; i < 7; i++) {
+    console.log(`${ i }: ${ categories[ i - 1 ] } to ${ categories[ i ] } FREE: 1 in ${ odds[ i ]}`)
   }
 
-  console.log(categories)
-  console.log(odds)
+  // Log the odds for category 7
+  console.log(`7: >=${ categories[ 7 ] } FREE: 1 in ${ odds[ 7 ]}`)
 }
 
 try {
